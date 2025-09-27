1654293332b35f73c0086d4b00a3528765e3adc3
jangan-resign <behagroup@gmail.com>
2025-09-27 12:53:50 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll untuk anchor link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+// Pop-up notifikasi download
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  alert("E-book gratis ini hanya bisa didownload di janganresign.fun ✨");
+});
